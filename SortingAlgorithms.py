d462160fcc4a4fdd17bbe9737ca96c9ab70ac7af
jutoart <jutoart@gmail.com>
2015-11-03 17:40:37 +0800

    Add new test method and performance measurement

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -4,6 +4,13 @@
 # Author: Art.Huang
 
 import sys
+import random
+from timeit import default_timer as timer
+
+def MeasureTime(nums, func):
+    time = timer()
+    sortedNums = func(nums)
+    return timer()-time, sortedNums
 
 def Ascending(x, y):
     return x - y
@@ -35,13 +42,53 @@ def InsertionSort(nums, compare = Ascending):
     return nums if len(nums) <= 1 else _Insert(nums, compare)
 
 
-if len(sys.argv) < 2:
-    print('Usage: SortingAlgorithms.py <input list of numbers>')
+if len(sys.argv) != 2:
+    print('Usage: SortingAlgorithms.py <test list size>')
     exit()
 
-print('Bubble Sort:')
-print(BubbleSort([int(n) for n in sys.argv[1:]]))
-print('Selection Sort:')
-print(SelectionSort([int(n) for n in sys.argv[1:]]))
-print('Insertion Sort:')
-print(InsertionSort([int(n) for n in sys.argv[1:]]))
+visibleSize = 30
+size = int(sys.argv[1])
+
+print('\n[Nearly Sorted]')
+nums = list(range(size))
+
+if size > 1:
+    nums[int(size/2)-1], nums[int(size/2)] = nums[int(size/2)], nums[int(size/2)-1]
+
+print('' + str(nums)) if size <= visibleSize else None
+t, s = MeasureTime(nums, BubbleSort)
+print('Bubble Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+t, s = MeasureTime(nums, SelectionSort)
+print('Selection Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+t, s = MeasureTime(nums, InsertionSort)
+print('Insertion Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+
+print('\n[Reverse Sorted]')
+nums = list(range(size, 0, -1))
+print('' + str(nums)) if size <= visibleSize else None
+t, s = MeasureTime(nums, BubbleSort)
+print('Bubble Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+t, s = MeasureTime(nums, SelectionSort)
+print('Selection Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+t, s = MeasureTime(nums, InsertionSort)
+print('Insertion Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+
+print('\n[Random Order]')
+nums = list(range(size))
+random.shuffle(nums)
+print('' + str(nums)) if size <= visibleSize else None
+t, s = MeasureTime(nums, BubbleSort)
+print('Bubble Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+t, s = MeasureTime(nums, SelectionSort)
+print('Selection Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
+t, s = MeasureTime(nums, InsertionSort)
+print('Insertion Sort:  ' + ('%.4f' % t) + ' secs')
+print(s) if size <= visibleSize else None
