e09d3d68dd1ef31a0352bac89ee3a43430a6beb6
jutoart <jutoart@gmail.com>
2015-11-20 19:44:34 +0800

    Add non-recursive version of tree traversal using stack and increase test case size

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -15,11 +15,40 @@ def InOrder(node):
         print(node.val)
         InOrder(node.right)
 
+def InOrderStack(node):
+    stack = []
+
+    while True:
+        if node:
+            stack.append(node)
+            node = node.left
+        else:
+            if len(stack) > 0:
+                node = stack.pop()
+                print(node.val)
+                node = node.right
+            else:
+                break
+
 def PreOrder(node):
     if node:
         print(node.val)
         PreOrder(node.left)
         PreOrder(node.right)
+
+def PreOrderStack(node):
+    stack = []
+    stack.append(node) if node else None
+
+    while len(stack) > 0:
+        node = stack.pop()
+        print(node.val)
+
+        if node.right:
+            stack.append(node.right)
+
+        if node.left:
+            stack.append(node.left)
 
 def PostOrder(node):
     if node:
@@ -27,13 +56,47 @@ def PostOrder(node):
         PostOrder(node.right)
         print(node.val)
 
-root = TreeNode(2)
-root.left = TreeNode(1)
-root.right = TreeNode(3)
+def PostOrderStack(node):
+    stack = []
+
+    while True:
+        if node:
+            stack.append(node.right) if node.right else None
+            stack.append(node)
+            node = node.left
+        else:
+            if len(stack) > 0:
+                node = stack.pop()
+
+                if node.right and len(stack) > 0 and stack[-1] is node.right:
+                    stack.pop()
+                    stack.append(node)
+                    node = node.right
+                else:
+                    print(node.val)
+                    node = None
+
+            else:
+                break
+
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(6)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left = TreeNode(5)
+root.right.right = TreeNode(7)
 print('In-order tree traversal:')
 InOrder(root)
 print('Pre-order tree traversal:')
 PreOrder(root)
 print('Post-order tree traversal:')
 PostOrder(root)
+print('In-order tree traversal using stack:')
+InOrderStack(root)
+print('Pre-order tree traversal using stack:')
+PreOrderStack(root)
+print('Post-order tree traversal using stack:')
+PostOrderStack(root)
 
